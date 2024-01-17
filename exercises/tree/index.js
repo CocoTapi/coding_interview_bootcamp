eb70078b16fa9beb17d08bd1eecb90e4175525cd
Chocotapi <shioriavino@gmail.com>
2024-01-17 15:09:00 -0500

    clean the code

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -11,44 +11,11 @@
 // function that gets called with each element in the tree
 
 class Node {
-    constructor(data){
-        this.data = data;
-        this.children = [];
-    }
-    add(data) {
-        const newNode = new Node(data);
-        this.children.push(newNode);
-    }
-    remove(data) {
-        this.children = this.children.filter(node => {
-            return node.data !== data;
-        });
-    }
+    
 }
 
 class Tree {
-    constructor() {
-        this.root = null;
-    }
-    traverseBF(fn) {
-        const arr = [this.root];
-        while (arr.length) {
-            const node = arr.shift();
-
-            //you can do this using for loop
-            arr.push(...node.children);
-            fn(node);
-        }
-    }
-    traverseDF(fn) {
-        const arr = [this.root];
-        while (arr.length) {
-            const node = arr.shift();
-
-            arr.unshift(...node.children);
-            fn(node);
-        }
-    }
+   
 }
 
 module.exports = { Tree, Node };

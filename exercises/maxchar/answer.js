bf5e267746047bf72d76d75cd10e254dc883b0ab
Chocotapi <shioriavino@gmail.com>
2024-01-24 15:57:45 -0500

    added pseudocode

--- exercises/maxchar/answer.js
+++ exercises/maxchar/answer.js
@@ -5,6 +5,22 @@
     - Does the given string have any repeated characters in it?
  */
 
+    
+    /*
+0. fix all letter to lowercase and remove the space. 
+1. if the length of the string is less than two, return the character.
+2. make an object to store the letters
+3. make a variable called maxCharValue to store the maximum count of any character.
+4. make a variable called maxChar to store the character with the maximum count
+5. iterate through each character of fixed string.
+    a. update the count of the current character in the object.
+    b. if the count of current character is greater than the current maximum count
+        i. update the count of maximum character
+        ii. set the character as the maximum character 
+4. return the maximum character
+*/
+
+
     function maxChar(str) {
         const charMap = {};
         let maxCharValue = 0;

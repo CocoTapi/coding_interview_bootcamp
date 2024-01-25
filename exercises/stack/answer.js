8f7bff1959502f14842d4e14af0563910e8487b2
Chocotapi <shioriavino@gmail.com>
2024-01-24 23:17:48 -0500

    added another answer

--- exercises/stack/answer.js
+++ exercises/stack/answer.js
@@ -13,3 +13,19 @@ class Stack {
         return dataArr[dataArr.length - 1];
     }
 }
+
+//2
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+    push(val) {
+        this.data.unshift(val);
+    }
+    pop() {
+        return this.data.shift();
+    }
+    peek() {
+        return this.data[0];
+    }
+}
